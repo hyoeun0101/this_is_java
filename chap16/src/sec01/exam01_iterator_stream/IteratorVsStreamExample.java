4932484ea68cccfed556ee527d67bd61ddbf689a
hyoeun0101 <kimhyoeun0101@gmail.com>
2022-06-11 23:52:35 +0900

    add chap12

--- chap16/src/sec01/exam01_iterator_stream/IteratorVsStreamExample.java
+++ chap16/src/sec01/exam01_iterator_stream/IteratorVsStreamExample.java
@@ -1,5 +1,17 @@
 package sec01.exam01_iterator_stream;
 
-public class IteratorVsStreamExample {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
+public class IteratorVsStreamExample {
+	List<String> list = Arrays.asList("hong","sin","gam");
+	
+	Iterator<String> iter = list.iterator();
+	while(iter.hasNext()) {
+		String name = iter.next();
+		System.out.println(name);
+	}
+	
+}
 }
